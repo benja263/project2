a9d801f0bd286af37ae1a315b68bf72d46e60c7f
QM Nguyen <qmdotn@gmail.com>
2017-12-18 08:48:21 +0100

    Fix typo in baseline/build_data.sh

--- src/baseline/build_data.sh
+++ src/baseline/build_data.sh
@@ -5,5 +5,5 @@
 python3 pickle_vocab.py
 python3 cooc.py
 python3 glove_solution.py
-python3 build_train_feature.py
-python3 build_test_feature.py
+python3 build_train_feature_matrix.py
+python3 build_test_feature_matrix.py
